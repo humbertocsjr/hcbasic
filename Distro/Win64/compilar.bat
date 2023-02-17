e7c520761694fe92a18237d363662de830b61a1a
Humberto Costa <humberto.costa@me.com>
2023-02-16 22:32:00 -0300

    v1.0.4-fix-pack

---
+++ Distro/Win64/compilar.bat
@@ -0,0 +1,5 @@
+hcbasic helloworld.hcb helloworld.asm Plataformas\8086-DOS\ Plataformas\8086\
+nasm -f bin -o helloworld.com helloworld.asm
+
+hcbasic gui.hcb gui.asm Plataformas\8086-DOS\ Plataformas\8086\
+nasm -f bin -o gui.com gui.asm
